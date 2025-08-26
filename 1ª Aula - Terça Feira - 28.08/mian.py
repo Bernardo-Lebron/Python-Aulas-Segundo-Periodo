83015782e8394ca29ece009e0007a200bcc648e7
Bernardo Lebron <bernardolmachado@exemplo.com>
2025-08-26 13:43:06 -0300

    Primeiro commit - Aulas de Python 2º período

---
+++ 1ª Aula - Terça Feira - 28.08/mian.py
@@ -0,0 +1,31 @@
+'''Primeiro programa'''
+
+nome = 'Bernardo Lebron'
+
+idade = 21
+
+print(f"{nome} tem {idade} anos!")
+
+valor = 123.456
+
+print(f"Valor formatado: {valor:.1f}")
+
+valor1 = 0.5
+
+print(f"Valor formatado: {valor1:.1%}")
+
+print(f"Valor formatado: {valor1:>6}")
+
+print(f"Valor formatado: {valor1:^6}")
+
+print(f"Valor formatado: {valor1:<6}")
+
+print(f"Valor formatado: {valor1:-^9}")
+
+print("Olá, {}!" .format(nome))
+
+print("O aluno {} tirou nota {}." .format(nome, 9.5))
+
+print("Nome: {0}, Nota {1}, \nNome novamente: {0}" .format(nome, 9.5))
+
+print("Valor {}".format(valor))
