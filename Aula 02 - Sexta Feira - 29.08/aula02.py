87b1f09473d657e64ce2cf0b920420816466dd96
Bernardo Lebron <bernardolmachado@exemplo.com>
2025-08-29 13:41:01 -0300

    Atualizacao automatica

--- Aula 02 - Sexta Feira - 29.08/aula02.py
+++ Aula 02 - Sexta Feira - 29.08/aula02.py
@@ -1,1 +1,52 @@
-print(f"Hello World!")
+from decimal import Decimal
+
+
+#####
+
+nome = 'Lebron'
+idade = 21
+
+print("Idade:" + str(idade), "\nNome:" + nome, end="\n\n") 
+
+#####
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(f"{nome}, tem {idade} anos\n")
+
+#####
+
+letra = 'A'
+
+print(letra)
+print(type(letra))
+print(len(letra), end="\n\n")
+
+#####
+
+x = 10
+
+print(x)
+print(x>5)
+print(x==0, end="\n\n")
+
+#####
+
+pi = 3.14159
+
+print(type(pi))
+print(pi, end="\n\n")
+
+#####
+
+y = Decimal("0.1") + Decimal("0.2")
+
+print(f"{y:.10f}")
+
+z = 0.1 + 0.2
+
+print(f"{z:.10f}", end="\n\n")
+
+#####
+
