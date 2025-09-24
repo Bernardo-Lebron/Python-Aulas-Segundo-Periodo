78f5d2bf62f1d2fadfa66aac63925f894473a897
Bernardo Lebron <bernardolmachado@gmail.com>
2025-09-24 14:29:20 -0300

    Atualizacao automatica em 24/09/2025 14:29:20,37

--- atualizar_git_auto.bat
+++ atualizar_git_auto.bat
@@ -1,7 +1,7 @@
 @echo off
 cd "C:\Users\berna\OneDrive\Área de Trabalho\2 Periodo\Python"
 git add .
-git commit -m "Atualizacao automatica"
+git commit -m "Atualizacao automatica em %date% %time%"
 git push origin main
 pause
 
