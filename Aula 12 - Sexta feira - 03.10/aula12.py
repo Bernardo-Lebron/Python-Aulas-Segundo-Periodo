43d5b2351c970a527861285db82e4742d3f17d93
Bernardo Lebron <bernardolmachado@gmail.com>
2025-10-03 13:16:45 -0300

    Atualizacao automatica em 03/10/2025 13:16:45,95

---
+++ Aula 12 - Sexta feira - 03.10/aula12.py
@@ -0,0 +1,67 @@
+'''
+#exe01
+rios = {
+    "Rio Nilo" : "Egito",
+    "Rio Amazonas" : "Brasil",
+    "Rio Yangtzé" : "China"
+}
+
+for rio, pais in rios.items():
+    print(f"O {rio} esta localizado no país: {pais}!".format())
+
+for rio in rios.keys():
+    print(rio)
+
+for pais in rios.values():
+    print(pais)
+
+#exe02
+
+respostas = {
+    "João": "python",
+    "Maria": "C",
+    "Pedro": "Java",
+}
+
+pessoas = ['João', 'Ana', 'Pedro', 'Carla', 'Maria', 'Beatriz']
+
+for pessoa in pessoas:
+    if pessoa in respostas:
+        print(f"Obrigado por participar da pesquisa {pessoa}!")
+    else:
+        print(f"{pessoa}, você ainda não respondeu a enquete")
+
+#exe03
+
+Nick = {
+    "tipo" : "Cachorro",
+    "dono" : "Lebron"
+}
+
+Geremias = {
+    "tipo" : "Gato",
+    "dono" : "Rafaela"
+}
+
+Sextafeira = {
+    "tipo" : "Papagaio",
+    "dono" : "Lica"
+}
+
+pets = [Nick, Geremias, Sextafeira]
+
+for pet in pets:
+    print(f"{pet["dono"]} tem um {pet["tipo"]}".format())
+'''
+#exe04
+
+favorite_places = {
+    "Bernardo": ["Carmópolis", "Belo Horizonte", "Maceió"],
+    "Rafaela": ["Carmópolis", "Salvador", "Rio de Janeiro"],
+    "Nick": ["Quintal", "Terreiro", "Roça"]
+}
+
+for nome, lugares in favorite_places.items():
+    print(f"\nOs lugares favoritos de {nome} são: ")
+    for lugar in lugares:
+        print(f"- {lugar}")
