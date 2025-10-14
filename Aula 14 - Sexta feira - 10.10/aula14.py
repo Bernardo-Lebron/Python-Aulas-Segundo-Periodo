ce342d5a9538986aaec23e403cab61223a2e4afe
Bernardo Lebron <bernardolmachado@gmail.com>
2025-10-14 09:05:14 -0300

    Atualizacao automatica em 14/10/2025  9:05:13,98

--- Aula 14 - Sexta feira - 10.10/aula14.py
+++ Aula 14 - Sexta feira - 10.10/aula14.py
@@ -1,4 +1,5 @@
-'''#exe01
+#exe01
+
 def make_car(marca, modelo, **car_info):
     """Constrói um dicionário que descreve um carro."""
     
@@ -13,17 +14,5 @@ def make_car(marca, modelo, **car_info):
 
 car = make_car('subaru', 'outback', color='blue', tow_package=True)
 print(car)
-'''
-#exe02
 
-i = input("Informe a quantidade de alunos que deseja lançar as notas: ")
-for i in range(int(i)):
-    nome = input("Nome do aluno: ")
-    nota = input("Nota do aluno: ")
-    notas_alunos = {
-        "nome": nome,
-        "nota": nota,
-    }
 
-for i in range(int(i)):
-    print(notas_alunos)
