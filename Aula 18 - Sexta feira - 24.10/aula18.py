0a32783b43598ed511b25aa77e9790478cae5e9b
Bernardo Lebron <bernardolmachado@gmail.com>
2025-10-24 12:10:34 -0300

    Atualizacao automatica em 24/10/2025 12:10:34,62

--- Aula 18 - Sexta feira - 24.10/aula18.py
+++ Aula 18 - Sexta feira - 24.10/aula18.py
@@ -1,5 +1,12 @@
 #exe01
-
+while True:
+    try:
+        n1 = float(input("Digite o primeiro número: "))
+        n2 = float(input("Digite o segundo número: "))
+        soma = n1 + n2
+        print(f"A soma de {n1} e {n2} é {soma}.")
+    except ValueError:
+        print("Por favor, insira valores numéricos válidos.")
 
 #exe02
 
