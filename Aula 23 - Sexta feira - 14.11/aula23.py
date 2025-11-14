f2ed5b4afe7e6fbd690f18d718fd765fc32c8d5f
Bernardo Lebron <bernardolmachado@gmail.com>
2025-11-14 12:13:07 -0300

    Atualizacao automatica em 14/11/2025 12:13:07,76

--- Aula 23 - Sexta feira - 14.11/aula23.py
+++ Aula 23 - Sexta feira - 14.11/aula23.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-a = np.array([[1,2,3],
-              [4,5,6]])
+a = np.array([[[1,2,3],
+              [4,5,6],
+              [7,8,9]]])
 
 print(a[0])
 
 b =  np.array([2., 2., 3.2, 4.])
 
-print(a[:1])
+print(b[:1])
+
+c = np.linspace(1,10,4)
+
+print(c)
