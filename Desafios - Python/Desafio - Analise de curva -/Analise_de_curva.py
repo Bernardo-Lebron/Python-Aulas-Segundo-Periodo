377b8e50431f920198846c3fbf6e50d7a035718d
Bernardo Lebron <bernardolmachado@gmail.com>
2025-11-28 11:00:50 -0300

    Atualizacao automatica em 28/11/2025 11:00:50,81

--- Desafios - Python/Desafio - Analise de curva -/Analise_de_curva.py
+++ Desafios - Python/Desafio - Analise de curva -/Analise_de_curva.py
@@ -9,12 +9,12 @@ np.random.seed(0)
 
 t = np.linspace(0, 10, 50)  
 
-A_real = 5
-k_real = 0.8
-C_real = 1
+A = 5
+k = 0.8
+C = 1
 
 ruido = np.random.normal(0, 0.5, size=len(t))
-y = modelo(t, A_real, k_real, C_real) + ruido
+y = modelo(t, A, k, C) + ruido
 
 chute_inicial = [4, 1, 0]
 
@@ -22,20 +22,18 @@ param_est, _ = curve_fit(modelo, t, y, p0=chute_inicial)
 
 A_est, k_est, C_est = param_est
 
-erro_A = abs(A_est - A_real)
-erro_k = abs(k_est - k_real)
-erro_C = abs(C_est - C_real)
+erro_A = abs(A_est - A)
+erro_k = abs(k_est - k)
+erro_C = abs(C_est - C)
 
 print("Parâmetros estimados:")
-print(f"A = {A_est:.4f} (erro = {erro_A:.4f})")
-print(f"k = {k_est:.4f} (erro = {erro_k:.4f})")
-print(f"C = {C_est:.4f} (erro = {erro_C:.4f})")
+print(f"A = {A_est} (erro = {erro_A})")
+print(f"k = {k_est} (erro = {erro_k})")
+print(f"C = {C_est} (erro = {erro_C})")
 
 plt.figure(figsize=(9, 5))
 plt.scatter(t, y, label="Dados experimentais (com ruído)", marker='o')
-plt.plot(t, modelo(t, A_est, k_est, C_est),
-         label="Curva ajustada", linewidth=2)
-
+plt.plot(t, modelo(t, A_est, k_est, C_est), label="Curva ajustada", linewidth=2)
 plt.xlabel("t (s)")
 plt.ylabel("y(t)")
 plt.title("Ajuste de Curva Exponencial")
